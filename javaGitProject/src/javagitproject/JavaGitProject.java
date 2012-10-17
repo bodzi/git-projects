519f805f40f9a5e8f2d5cf82a9b532603ed375ee
bojana.manjak <bojana.manjak@bojanam-pc.soneco.co.rs>
2012-10-17 16:38:20 +0200

    Hello world I am Anaconda I am Bambus

--- javaGitProject/src/javagitproject/JavaGitProject.java
+++ javaGitProject/src/javagitproject/JavaGitProject.java
@@ -16,5 +16,6 @@ public class JavaGitProject {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
          System.out.println("I am Anakonda");
+          System.out.println("I am Bambus the Anakonda sister");
     }
 }
