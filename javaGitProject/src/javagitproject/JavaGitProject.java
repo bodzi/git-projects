8dad477f239c6d8af86632c2b8c6609af47a3a6d
bojana.manjak <bojana.manjak@bojanam-pc.soneco.co.rs>
2012-10-17 15:55:49 +0200

    I am Bambus Anaconda's sister

--- javaGitProject/src/javagitproject/JavaGitProject.java
+++ javaGitProject/src/javagitproject/JavaGitProject.java
@@ -14,6 +14,6 @@ public class JavaGitProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("I am Anakonda");
+        System.out.println("I am Bambus the Anakonda sister");
     }
 }
